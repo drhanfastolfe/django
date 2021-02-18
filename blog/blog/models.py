e9a6cbf69e3a00efd62c2656edd7a20f1e865789
Hamza Mohammad <hamzzza56@gmail.com>
2021-02-18 19:45:03 +0100

    Añade fomulario para nuevos posts

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-# Create your models here.
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(
@@ -18,3 +17,6 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
